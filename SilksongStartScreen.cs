0158d5d3a69a557519585a44911b9cdf23720244
Ngo Phu Hien <ngophuhien029@gmail.com>
2025-08-28 15:12:58 +0700

    Custom menu title based on menu style + Add Copyright

--- SilksongStartScreen.cs
+++ SilksongStartScreen.cs
@@ -10,12 +10,15 @@ using System.Reflection;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Diagnostics;
+using Newtonsoft.Json;
 
 namespace CustomTitle
 {
     public class CustomTitle : Mod
     {
         public override string GetVersion() => System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        readonly string modDataDir = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "data");
 
         public CustomTitle() : base("Custom Title")
         {
@@ -24,15 +27,19 @@ namespace CustomTitle
 
         private void FixMenuTitle(On.MenuStyleTitle.orig_SetTitle orig, MenuStyleTitle self, int index)
         {
-            MenuStyleTitle.TitleSpriteCollection spriteCollection =
-                index < 0 || index >= self.TitleSprites.Length
-                    ? self.DefaultTitleSprite
-                    : self.TitleSprites[index];
+            byte[] fileData;
 
-            string modDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            string path = Path.Combine(modDir, "logo.png");
-
-            byte[] fileData = File.ReadAllBytes(path);
+            try 
+            {
+                fileData = File.ReadAllBytes(GetImagePath());
+            }
+            catch (Exception ex)
+            {
+                Log("Failed to load image. " + ex.Message);
+                Log("Switching to default menu title");
+                orig(self, index);
+                return;
+            }
 
             Texture2D RealTitle_texture = new Texture2D(2, 2);
             RealTitle_texture.LoadImage(fileData);
@@ -41,12 +48,27 @@ namespace CustomTitle
                 RealTitle_texture,
                 new Rect(0, 0, RealTitle_texture.width, RealTitle_texture.height),
                 new Vector2(0.5f, 0.5f),
-                spriteCollection.Default.pixelsPerUnit,
+                RealTitle_texture.height / 8,
                 0,
                 SpriteMeshType.FullRect
             );
 
             self.Title.sprite = RealTitle;
+        }
+
+        private string GetImagePath()
+        {
+            Log(Application.persistentDataPath);
+            string path = Path.Combine(modDataDir, "data.json");
+            string json = File.ReadAllText(path);
+            Dictionary<string, string> dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+
+            return Path.Combine(modDataDir, dict[GetActiveMenuStyle()]);
+        }
+
+        private string GetActiveMenuStyle()
+        {
+            return MenuStyles.Instance.styles[MenuStyles.Instance.CurrentStyle].styleObject.name;
         }
     }
 }
